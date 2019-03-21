fc40671dd4266320b58cc196a06f42f6e0a7e292
IrinaSiandetskaya <irene_siv@inbox.ru>
2019-03-21 17:06:15 +0300

    realised by function clear completed tasks and count tasks

--- add_tasks.js
+++ add_tasks.js
@@ -1,24 +1,36 @@
-var arrayTasks = new Array();
+var arrayTasks = [];
 
-function addTask() {
-    document.querySelector('input').addEventListener('keydown', function (e) {
-        if (e.keyCode === 13) {
-            arrayTasks.unshift(document.getElementsByClassName("task_add_new")[0].value);
-            document.getElementsByClassName('task_label_result')[0].innerHTML = arrayTasks;
-            // arrayTasks.forEach(element => {
-            //     document.getElementById('resultTask').innerHTML = arrayTasks[element];
-            // }); 
-            // for(var element in arrayTasks){
-            //     document.getElementById('resultTask').innerHTML = arrayTasks[0];
-            // }
-            document.getElementsByClassName("task_add_new")[0].value = "";
-            alert("Pressed " + arrayTasks);
-        }
-    });
-};
+function addTask(e) {
+
+    if (e.keyCode === 13) {
+        arrayTasks.unshift(document.getElementsByClassName("task_add_new")[0].value);
+
+        showTask();
+        document.getElementsByClassName("task_add_new")[0].value = "";
+        console.log("Pressed " + arrayTasks);
+        console.log("CountTasks " + arrayTasks.length);
+        document.getElementsByClassName('task_label_count')[0]
+        .innerHTML = arrayTasks.length;
+    }
+
+}
 
 function showTask() {
+
     for (var i = 0; i < arrayTasks.length; i++) {
-        document.getElementById('resultTask').innerHTML = arrayTasks[i];
+        document.getElementsByClassName('task_label_result')[0]
+            .innerHTML = arrayTasks;
+    }
+}
+
+function deleteCompletedTask() {
+    
+    var container = document.querySelectorAll('.task_completed');
+
+    for(var i=0; i<container.length;i++){
+        var childTask=container[i].firstElementChild;
+        if(childTask.matches(':checked')){
+            var deleteElement = container[i].remove();
+        }
     }
 }
